eccb21616ba39d6a0aefd66d999c28f5a6b68171
Diogo Biazus <diogob@gmail.com>
2011-06-09 17:04:31 -0300

    added earth_distance second check

--- lib/activerecord-postgres-earthdistance/activerecord.rb
+++ lib/activerecord-postgres-earthdistance/activerecord.rb
@@ -8,7 +8,9 @@ module ActiveRecord
       @@longitude_column = (column_names.include?("lng") ? "lng" : "longitude") unless @@longitude_column
 
       def self.within_radius radius, lat, lng, unit = :meters
-        where(["ll_to_earth(#{@@latitude_column}, #{@@longitude_column}) <@ earth_box(ll_to_earth(?, ?), ?)", radius, lat, lng])
+        where(["ll_to_earth(#{@@latitude_column}, #{@@longitude_column}) <@ earth_box(ll_to_earth(?, ?), ?)
+               AND earth_distance(ll_to_earth(#{@@latitude_column}, #{@@longitude_column}), ll_to_earth(?, ?)) <= ?", 
+               radius, lat, lng, radius, lat, lng])
       end
     end
   end
