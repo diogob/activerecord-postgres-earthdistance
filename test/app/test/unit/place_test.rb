3a571f60c4a78d92bc7fd226ed3207ff06f54e72
Diogo Biazus <diogob@gmail.com>
2011-06-09 15:22:15 -0300

    just tidying up the tests

--- test/app/test/unit/place_test.rb
+++ test/app/test/unit/place_test.rb
@@ -5,12 +5,18 @@ class PlaceTest < ActiveSupport::TestCase
     @test_place = Place.create!(:lat => -30.0277041, :lng => -51.2287346)
   end
 
-  test "the truth" do
+  test "can find place within radius" do
     assert_equal [@test_place], Place.within_radius(-30.0277041, -51.2287346, 0).all 
-    assert_equal [], Place.within_radius(-27.5969039, -48.5494544, 0).all
-    assert_equal [], Place.within_radius(-27.5969039, -48.5494544, 1000).all
     assert_equal [@test_place], Place.within_radius(-27.5969039, -48.5494544, 400000).all
     assert_equal [@test_place], Place.within_radius(-27.5969039, -48.5494544, 4000000).all
+  end
+  test "won't find the place if radius is 0" do
+    assert_equal [], Place.within_radius(-27.5969039, -48.5494544, 0).all
+  end
+  test "won't find places outside the radius" do
+    assert_equal [], Place.within_radius(-27.5969039, -48.5494544, 1000).all
+  end
+  test "can handle more than one place" do
     another_place = Place.create! :lat => -27.5969039, :lng => -48.5494544
     assert_equal [@test_place], Place.within_radius(-30.0277041, -51.2287346, 0).all
     assert_equal [another_place], Place.within_radius(-27.5969039, -48.5494544, 0).all
