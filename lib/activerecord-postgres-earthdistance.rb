34e698b445097dab82b7d0dc5f1e0c5f5890157b
Diogo Biazus <diogob@gmail.com>
2013-02-07 11:49:00 -0200

    put railties in separate file and include only when rails is present

--- lib/activerecord-postgres-earthdistance.rb
+++ lib/activerecord-postgres-earthdistance.rb
@@ -1,46 +1,9 @@
-require 'rails'
-require 'rails/generators'
-require 'rails/generators/migration'
+require 'active_support'
 
-# = Earth Distance Railtie
-#
-# Creates a new railtie for 2 reasons:
-#
-# * Initialize ActiveRecord properly
-# * Add earthdistance:setup generator 
-class EarthDistance < Rails::Railtie
-
-  initializer 'activerecord-postgres-earthdistance' do
-    ActiveSupport.on_load :active_record do
-      require "activerecord-postgres-earthdistance/activerecord"
-    end
-  end
-
-  # Creates the earthdistance:setup generator. This generator creates a migration that
-  # adds earthdistance support for your database. If fact, it's just the sql from the
-  # contrib inside a migration. But it' s handy, isn't it?
-  #
-  # To use your generator, simply run it in your project:
-  #
-  #   rails g earthdistance:setup
-  class Setup < Rails::Generators::Base
-    include Rails::Generators::Migration
-
-    def self.source_root
-      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-    end
-
-    def self.next_migration_number(dirname)
-      if ActiveRecord::Base.timestamped_migrations
-        Time.now.utc.strftime("%Y%m%d%H%M%S")
-      else
-        "%.3d" % (current_migration_number(dirname) + 1)
-      end
-    end
-
-    def create_migration_file
-      migration_template 'setup_earthdistance.rb', 'db/migrate/setup_earthdistance.rb'
-    end
-
+if defined? Rails
+  require "activerecord-postgres-earthdistance/railties"
+else
+  ActiveSupport.on_load :active_record do
+    require "activerecord-postgres-earthdistance/activerecord"
   end
 end
