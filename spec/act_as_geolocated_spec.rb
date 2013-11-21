96a9c4eb30fd60505a2b47cf9c0359e8b5fd662a
Andre Kramer <andre@wirelab.nl>
2013-11-21 17:20:06 +0100

    order records based on distance of a point

--- spec/act_as_geolocated_spec.rb
+++ spec/act_as_geolocated_spec.rb
@@ -8,6 +8,10 @@ describe "ActiveRecord::Base.act_as_geolocated" do
       @place = Place.create!(:lat => -30.0277041, :lng => -51.2287346)
     end
 
+    after(:all) do
+      @place.destroy
+    end
+
     context "when query with null data" do
       it{ should == [] }
     end
@@ -24,6 +28,29 @@ describe "ActiveRecord::Base.act_as_geolocated" do
 
     context "when query for place outside the radius" do
       let(:test_data){ {radius: 1000, lat: -27.5969039, lng: -48.5494544} }
+    end
+  end
+
+  describe "#order_by_distance" do
+    let(:current_location){ {lat: nil, lng: nil, radius: nil} }
+    subject{ Place.order_by_distance(current_location[:lat], current_location[:lng]) }
+    before(:all) do
+      @place_1 = Place.create!(:lat => 52.370216, :lng => 4.895168) #Amsterdam
+      @place_2 = Place.create!(:lat => 52.520007, :lng => 13.404954) #Berlin
+    end
+    after(:all) do
+      @place_1.destroy
+      @place_2.destroy
+    end
+
+    context "when sorting on distance" do
+      let(:current_location){{lat: 51.511214, lng: 0.119824}} #London
+      it{ should == [@place_1, @place_2] }
+    end
+
+    context "when sorting on distance from another location" do
+      let(:current_location){{lat: 52.229676, lng: 21.012229}} #Warsaw
+      it{ should == [@place_2, @place_1] }
     end
   end
 end
