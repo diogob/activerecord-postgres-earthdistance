825ae533defe2cd84d3e705844c79858c56ebe1c
Michał Matyas <github@higher.lv>
2014-12-09 11:45:44 +0100

    Rewrite gem to use Arel instead of contatenating strings

--- lib/activerecord-postgres-earthdistance/acts_as_geolocated.rb
+++ lib/activerecord-postgres-earthdistance/acts_as_geolocated.rb
@@ -2,9 +2,6 @@ module ActiveRecordPostgresEarthdistance
   module ActsAsGeolocated
     extend ActiveSupport::Concern
 
-    included do
-    end
-
     module ClassMethods
       def acts_as_geolocated(options = {})
         if table_exists?
@@ -14,16 +11,28 @@ module ActiveRecordPostgresEarthdistance
         end
       end
 
-      def within_box radius, lat, lng
-        where("ll_to_earth(#{self.latitude_column}, #{self.longitude_column}) <@ earth_box(ll_to_earth(?, ?), ?)", lat, lng, radius)
+      def within_box(radius, lat, lng)
+        earth_box = Arel::Nodes::NamedFunction.new('earth_box', [ll_to_earth_coords(lat, lng), radius])
+        where Arel::Nodes::InfixOperation.new('<@', ll_to_earth_columns, earth_box)
       end
 
-      def within_radius radius, lat, lng
-        within_box(radius, lat, lng).where("earth_distance(ll_to_earth(#{self.latitude_column}, #{self.longitude_column}), ll_to_earth(?, ?)) <= ?", lat, lng, radius)
+      def within_radius(radius, lat, lng)
+        earth_distance = Arel::Nodes::NamedFunction.new('earth_distance', [ll_to_earth_columns, ll_to_earth_coords(lat, lng)])
+        within_box(radius, lat, lng).where(Arel::Nodes::InfixOperation.new('<=', earth_distance, radius))
       end
 
-      def order_by_distance lat, lng, order= "ASC"
-        order("earth_distance(ll_to_earth(#{self.latitude_column}, #{self.longitude_column}), ll_to_earth(#{lat}, #{lng})) #{order}")
+      def order_by_distance(lat, lng, order = "ASC")
+        earth_distance = Arel::Nodes::NamedFunction.new('earth_distance', [ll_to_earth_columns, ll_to_earth_coords(lat, lng)])
+        order("#{earth_distance.to_sql} #{order.to_s}")
+      end
+
+      protected
+      def ll_to_earth_columns
+        Arel::Nodes::NamedFunction.new('ll_to_earth', [arel_table[self.latitude_column], arel_table[self.longitude_column]])
+      end
+
+      def ll_to_earth_coords lat, lng
+        Arel::Nodes::NamedFunction.new('ll_to_earth', [lat, lng])
       end
     end
   end
@@ -33,12 +42,11 @@ module ActiveRecordPostgresEarthdistance
       clone.tap do |relation|
         values = []
         values << relation.arel_table[Arel.star] if relation.select_values.empty? && include_default_columns
-        values << "earth_distance(ll_to_earth(#{self.latitude_column}, #{self.longitude_column}), ll_to_earth(#{lat}, #{lng})) as #{name}"
+        values << "earth_distance(ll_to_earth(#{self.quoted_table_name}.#{self.latitude_column}, #{self.quoted_table_name}.#{self.longitude_column}), ll_to_earth(#{lat}, #{lng})) as #{name}"
         relation.select_values = values
       end
     end
   end
-
 end
 
 ActiveRecord::Base.send :include, ActiveRecordPostgresEarthdistance::ActsAsGeolocated
