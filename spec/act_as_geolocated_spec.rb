ed7db2ae04c4453db92530f267f6c4bf444ced72
Max Furman <max@arcweb.co>
2018-04-24 12:01:44 -0400

    no need to change spec_helper

--- spec/act_as_geolocated_spec.rb
+++ spec/act_as_geolocated_spec.rb
@@ -217,7 +217,6 @@ describe "ActiveRecord::Base.act_as_geolocated" do
     let(:test_data) { { lat: nil, lng: nil, radius: nil } }
     subject { Place.within_radius(test_data[:radius], test_data[:lat], test_data[:lng]) }
     before(:all) do
-      # Place.distance_unit = :miles
       Place.acts_as_geolocated distance_unit: :miles
       @place = Place.create!(lat: -30.0277041, lng: -51.2287346)
     end
